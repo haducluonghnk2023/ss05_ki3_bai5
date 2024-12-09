d55da6619b61baa66abb3f51e9ad820facd91278
haducluonghnk2023 <147390439+haducluonghnk2023@users.noreply.github.com>
2024-12-09 22:13:27 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void append(Node** head, int value) {
+    Node* newNode = createNode(value);
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void reverseList(Node** head) {
+    Node* current = *head;
+    Node* temp = NULL;
+
+    while (current != NULL) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+
+    if (temp != NULL) {
+        *head = temp->prev;
+    }
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    printf("NULL");
+    while (temp != NULL) {
+        printf(" <--> %d", temp->data);
+        temp = temp->next;
+    }
+    printf(" <--> NULL\n");
+}
+
+int main() {
+    Node* head = NULL;
+    int n, value;
+    printf("Nhap so luong phan tu ban dau: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Nhap gia tri phan tu thu %d: ", i + 1);
+        scanf("%d", &value);
+        append(&head, value);
+    }
+    reverseList(&head);
+    printf("Danh sach sau khi dao nguoc: ");
+    printList(head);
+
+    return 0;
+}
+
